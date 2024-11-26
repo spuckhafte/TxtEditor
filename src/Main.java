54cec8cbae5aaffe3b06df86a92e62675212aa43
spuckhafte <rakshit240405@gmail.com>
2024-11-27 04:36:10 +0530

    ign

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import helpers.components.TopMenu.GuiMenuBar;
 public class Main {
     public static void main(String[] args) {
         GuiFrame frame = new GuiFrame(
-                "Txt Editor",
+                "TxtEditor",
                 1200, 700
         );
 
